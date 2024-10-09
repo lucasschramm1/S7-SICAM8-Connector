5ec86e776bbcf2e770ab1257db59ea568170a118
lucasschramm1 <160025017+lucasschramm1@users.noreply.github.com>
2024-10-09 14:15:46 +0200

    Update TestSnap7 repository

---
+++ TestSnap7/src/Info.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Info {
+    static const std::string NrDBS8anS7 = "1";
+    static const std::string NrDBS7anS8 = "2";
+    static const std::string IPadresse = "192.168.1.1";
+}
