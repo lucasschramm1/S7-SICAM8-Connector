5bfa3bda29cf6d5a40e87550f83898f183ef2f57
lucasschramm1 <160025017+lucasschramm1@users.noreply.github.com>
2024-10-24 11:02:16 +0200

    Update Snap7

---
+++ Snap7/src/Info.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Info {
+    static const std::string DBS8anS7 = "1";
+    static const std::string DBS7anS8 = "2";
+    static const std::string IPadresse = "192.168.1.1";
+}
